54b5a340e8c33a00f437f294c7f7b0daff9d1fb8
hs122 <hs512128@gmail.com>
2023-04-07 20:23:02 +0530

    Umbrella Completes

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const deleteImage = document.querySelector(".delete_image");
 
 
 let imagesArray = [];
+// Loading And Umbrella Change
+const getSpecificImages = (color,value) => {
 
-const getSpecificImages = (color) => {
   setTimeout(() => {
     umbrellaImages.src = `images/${color} umbrella.png`;
     loadingIcon.setAttribute("style", "display:none");
@@ -19,13 +20,13 @@ const getSpecificImages = (color) => {
 
   loadingIcon.setAttribute("style", "display:block");
   document.body.style.background = `linear-gradient(${color} 0%, 5%, rgb(221 227 229 / 20%) 100%)`;
-  loadingIcon.style.fill = `${color}`;
+  loadingIcon.style.fill = `${value}`;
   umbrellaImages.setAttribute("src", "");
   outputLogo.setAttribute("style", "display:none");
- label.style.background= `${color}`;
+ label.style.background= `${value}`;
 };
-
-function displayImages(color) {
+//Click Upload Button - Show Image
+function displayImages() {
   let images = "";
   imagesArray.forEach((image) => {
     images += `<div class="image">
@@ -45,14 +46,17 @@ function displayImages(color) {
 
 
 }
-
+//Event-Listener(Click) - Umbrella Color
 umbrellaColors.forEach((item) => {
   item.addEventListener("click", (event) => {
+let color = item.attributes.class.value
+let value = item.attributes.value.value
     event.preventDefault();
-    getSpecificImages(item.getAttribute("class"));
+    getSpecificImages(color,value);
 
   });
 });
+//Event-Listener(Change) -Upload Logo
 uploadButtton.addEventListener("change", (event) => {
   event.preventDefault();
   const file = uploadButtton.files;
