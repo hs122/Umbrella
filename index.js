b7e80cbeb6e427f6505a2b3b2912bdc195c3f912
hs122 <hs512128@gmail.com>
2023-04-07 20:01:14 +0530

    Umbrella Complete

--- index.js
+++ index.js
@@ -4,50 +4,57 @@ const loadingIcon = document.querySelector(".rotate");
 const uploadButtton = document.querySelector("#upload");
 const outputLogo = document.querySelector("output");
 const label = document.querySelector("label");
-const icon = document.querySelector("#button__icon");
-console.log(uploadButtton);
+const deleteImage = document.querySelector(".delete_image");
+
+
 let imagesArray = [];
 
 const getSpecificImages = (color) => {
   setTimeout(() => {
-    umbrellaImages.src = `${color} umbrella.png`;
-    loadingIcon.style.display = "none";
-    outputLogo.style.display ="block"
-  }, 3000);
-  outputLogo.style.display ="none"
-  loadingIcon.style.display = "block";
-  document.body.style.background = `linear-gradient(${color} 0%, 0%, rgb(221 227 229 / 20%) 100%)`;
+    umbrellaImages.src = `images/${color} umbrella.png`;
+    loadingIcon.setAttribute("style", "display:none");
+    outputLogo.setAttribute("style", "display:block");
+
+  }, 2000);
+
+  loadingIcon.setAttribute("style", "display:block");
+  document.body.style.background = `linear-gradient(${color} 0%, 5%, rgb(221 227 229 / 20%) 100%)`;
   loadingIcon.style.fill = `${color}`;
-  umbrellaImages.src = "";
+  umbrellaImages.setAttribute("src", "");
+  outputLogo.setAttribute("style", "display:none");
+ label.style.background= `${color}`;
 };
 
-function displayImages() {
+function displayImages(color) {
   let images = "";
-  imagesArray.forEach((image, index) => {
-
+  imagesArray.forEach((image) => {
     images += `<div class="image">
-                        <img src="${URL.createObjectURL(image)}" >
+                        <img src="${URL.createObjectURL(image)}">
                       </div>`;
     label.innerHTML = image.name;
+    deleteImage.style.display = "block";
   });
   setTimeout(() => {
-    loadingIcon.style.display = "none";
+    loadingIcon.setAttribute("style", "display:none");
     outputLogo.innerHTML = images;
-    umbrellaImages.style.display ='block';
+    umbrellaImages.setAttribute("style", "display:block");
+  }, 2000);
 
-  }, 3000);
+  loadingIcon.setAttribute("style", "display:block");
+  umbrellaImages.setAttribute("style", "display:none");
 
-  loadingIcon.style.display = "block";
-  icon.innerHTML = loadingIcon.innerHTML;
-  umbrellaImages.style.display ='none';
+
 }
 
 umbrellaColors.forEach((item) => {
-  item.addEventListener("click", () => {
+  item.addEventListener("click", (event) => {
+    event.preventDefault();
     getSpecificImages(item.getAttribute("class"));
+
   });
 });
-uploadButtton.addEventListener("change", () => {
+uploadButtton.addEventListener("change", (event) => {
+  event.preventDefault();
   const file = uploadButtton.files;
   imagesArray.push(file[0]);
   displayImages();
